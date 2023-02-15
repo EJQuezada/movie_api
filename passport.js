822d2d6d8abf8302a4979fa872109c80666feee9
EJQuezada <115997231+EJQuezada@users.noreply.github.com>
2023-02-15 21:34:06 +0100

    Update passport.js

--- passport.js
+++ passport.js
@@ -9,7 +9,7 @@ let Users = Models.User,
 
 passport.use(new LocalStrategy({
     usernameField: 'Username',
-    passwordField: 'Password'
+    passwordField: 'Password',
 }, (username, password, callback) => {
     console.log(username + ' ' + password);
     Users.findOne({ Username: username }, (error, user) => 
@@ -24,6 +24,13 @@ passport.use(new LocalStrategy({
             return callback(null, false, {message: 'Incorrect username or password.'});
         }
 
+        if (!user.validatePassword(password)) {
+            console.log('incorrect password');
+            return callback(null, false, {
+                message: 'Incorrect password.',
+            });
+        }
+
         console.log('finished');
         return callback(null, user);
     });
@@ -31,13 +38,13 @@ passport.use(new LocalStrategy({
 
 passport.use(new JWTStrategy({
     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey: 'your_jwt_secret'
+    secretOrKey: 'your_jwt_secret',
 }, (jwtPayload, callback) => {
     return Users.findById(jwtPayload._id)
         .then((user) => {
             return callback(null, user);
         })
         .catch((error) => {
-            return callback(error)
+            return callback(error);
         });
 }));
