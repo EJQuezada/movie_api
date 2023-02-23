d4078c8002fe0ce24d7c8a50d6a4ab0657ad8ab3
EJQuezada <115997231+EJQuezada@users.noreply.github.com>
2023-02-23 13:08:10 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,22 +1,24 @@
-const jwtSecret = 'your_jwt_secret';
+const jwtSecret = 'your_jwt_secret'; //Make sure it is the same key used in passport.js
 
 const jwt = require('jsonwebtoken'),
     passport = require('passport');
 
-require('./passport');
+require('./passport'); //Local Passport file
 
 let generateJWTToken = (user) => {
     return jwt.sign(user, jwtSecret, {
         subject: user.Username, //This is the username being encoded in the JWT
-        expiresIn: '7d',
-        algorithm: 'HS256'
+        expiresIn: '7d', //Token expires after 7 days
+        algorithm: 'HS256',
     });
-}
+};
 
 /* POST login. */
 module.exports = (router) => {
     router.post('/login', (req, res) => {
         passport.authenticate('local', { session: false }, (error, user, info) => {
+            console.log(error) 
+            console.log(user)
             if (error || !user) {
                 return res.status(400).json({
                     message: 'Something is not right',
@@ -28,7 +30,7 @@ module.exports = (router) => {
                     res.send(error);
                 }
                 let token = generateJWTToken(user.toJSON());
-                return res.json({ user, token });
+                return res.json({user, token});
             });
         })(req, res);
     });
